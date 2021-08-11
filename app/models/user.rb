87722cb07b58848062594127c6d0939ce45eba5f
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-11 13:02:49 -0700

    add /admin page
    Co-authored-by: krishops <krishops@users.noreply.github.com>
    Co-authored-by: aracelivaldovinos <aracelivaldovinos@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :albums
+  
+  rails_admin do
+    configure :user do
+      label 'Owner of this ball: '
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: "registrations"}
   
   as :user do
