29ea16d0f7de4c0615cc6a216eaddd14226cb3d7
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-11 12:46:53 -0700

    add image loader to New Album page
    Co-authored-by: krishops <krishops@users.noreply.github.com>
    Co-authored-by: aracelivaldovinos <aracelivaldovinos@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :authenticate_user!
 
-  def before_sign_in_path_for(resource)
-    edit_user_registration_path  
+  def after_sign_in_path_for(resource)
+    albums_path
   end
-
-
-  # def after_sign_in_path_for(resource)
-  #   # albums_path
-  #   redirect_to ''
-  # end
   
 
   # def current_user
